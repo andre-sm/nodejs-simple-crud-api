9eab46dd4f085208caed9b821192f06c72c2eb07
--global <--global>
2024-02-07 19:19:34 +0300

    feat: use .env file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import http from 'http';
+import dotenv from 'dotenv';
 import { handleRequests } from './routes/routes';
+
+dotenv.config();
+const port = process.env.PORT || 4000;
 
 const server = http.createServer();
 server.on('request', (req, res) => handleRequests(req, res));
-server.listen(3000);
+server.listen(port);
