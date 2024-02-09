a290e4118cafb197e0b3eddb4fbec76236eb5b24
--global <--global>
2024-02-09 20:22:59 +0300

    feat: handle non-existing endpoint

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,39 +7,24 @@ const handleRequests = (req: IncomingMessage, res: ServerResponse): void => {
   try {
     const { url, method } = req;
     const urlParts = url?.split('/').filter(Boolean) as string[];
+    const clearUrl = url?.endsWith('/') ? url.slice(0, -1) : url;
+
+    const isValidURLStart = urlParts[0] === 'api' && urlParts[1] === 'users';
     const userId = urlParts[2];
 
-    switch (method) {
-      case 'GET':
-        if (url === '/api/users') {
-          getAllUsers(req, res);
-        }
-
-        if (urlParts[0] === 'api' && urlParts[1] === 'users' && userId) {
-          getUser(req, res, urlParts[2]);
-        }
-        break;
-
-      case 'POST':
-        if (url === '/api/users') {
-          addUser(req, res);
-        }
-        break;
-
-      case 'PUT':
-        if (urlParts[0] === 'api' && urlParts[1] === 'users' && userId) {
-          editUser(req, res, userId);
-        }
-        break;
-
-      case 'DELETE':
-        if (urlParts[0] === 'api' && urlParts[1] === 'users' && userId) {
-          deleteUser(req, res, userId);
-        }
-        break;
-
-      default:
-        break;
+    if (method === 'GET' && clearUrl === '/api/users') {
+      getAllUsers(req, res);
+    } else if (method === 'GET' && isValidURLStart && userId && urlParts.length === 3) {
+      getUser(req, res, userId);
+    } else if (method === 'POST' && clearUrl === '/api/users') {
+      addUser(req, res);
+    } else if (method === 'PUT' && isValidURLStart && userId && urlParts.length === 3) {
+      editUser(req, res, userId);
+    } else if (method === 'DELETE' && isValidURLStart && userId && urlParts.length === 3) {
+      deleteUser(req, res, userId);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end('Endpoint doesn\'t exist');
     }
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'application/json' });
