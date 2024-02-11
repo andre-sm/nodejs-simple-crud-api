d29b4f2c797b51358bf8dd171e18b1069332fc93
--global <--global>
2024-02-12 02:39:17 +0300

    feat: forward primary requests to workers

--- src/multi-server.ts
+++ src/multi-server.ts
@@ -1,32 +1,67 @@
-import http from 'http';
+import http, { IncomingMessage, ServerResponse } from 'http';
 import cluster from 'cluster';
 import os from 'os';
+import { handleRequests } from './routes/routes';
 
 const createMultiServer = (port: string | number) => {
-  const numberOfCPU = os.availableParallelism();
+  try {
+    const numberOfCPU = os.availableParallelism();
 
-  if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
+    if (cluster.isPrimary) {
+      console.log(`Primary ${process.pid} is running`);
+      const workerIDs: number[] = [];
+      let lastWorkerIndex = 0;
 
-    for (let i = 1; i < numberOfCPU; i += 1) {
-      cluster.fork();
-    }
+      for (let i = 1; i < numberOfCPU; i += 1) {
+        const worker = cluster.fork();
+        workerIDs.push(worker.id);
+      }
 
-    cluster.on('exit', (worker) => {
-      console.log(`worker ${worker.process.pid} died`);
-    });
+      cluster.on('exit', (worker) => {
+        console.log(`worker ${worker.process.pid} died`);
+      });
 
-    const server = http.createServer();
-    server.on('request', (req, res) => console.log(res));
-    server.listen(port);
-  } else {
-    console.log(`Worker ${process.pid} started`);
-
-    for (let i = 1; i < numberOfCPU; i += 1) {
       const server = http.createServer();
-      server.on('request', (req, res) => console.log(res));
-      server.listen(+port + i);
+      server.on('request', (originalReq: IncomingMessage, originalRes: ServerResponse) => {
+        const { pathname } = new URL(originalReq.url || '', `http://${originalReq.headers.host}`);
+        const newPort = (+port + lastWorkerIndex + 1).toString();
+
+        const requestOptions = {
+          hostname: 'localhost',
+          port: newPort,
+          path: pathname,
+          method: originalReq.method,
+          headers: { ...originalReq.headers, host: `localhost:${newPort}` },
+        };
+
+        const proxyRequest = http.request(requestOptions, (proxyResponse) => {
+          originalRes.writeHead(proxyResponse.statusCode as number, proxyResponse.headers);
+          proxyResponse.pipe(originalRes, { end: true });
+        });
+
+        originalReq.pipe(proxyRequest, { end: true });
+
+        lastWorkerIndex = (lastWorkerIndex === workerIDs.length - 1) ? 0 : lastWorkerIndex + 1;
+      });
+
+      server.listen(port, () => {
+        console.log(`Primary server listening on port ${port}`);
+      });
+    } else {
+      console.log(`Worker ${process.pid} started`);
+
+      const server = http.createServer();
+      server.on('request', (req, res) => {
+        handleRequests(req, res);
+      });
+
+      const workerId = cluster.worker?.id as number;
+      server.listen(+port + workerId, () => {
+        console.log(`Worker server listening on port ${+port + workerId}`);
+      });
     }
+  } catch (error) {
+    console.error();
   }
 };
 
